e3be936fd5d71b521b8a39bdfaacd9620aa12346
Cristian <cristian.rr99@gmail.com>
2023-03-14 17:02:34 -0300

    Nova especificação: mostrar o registro de operações

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
 
         {
+            string[] registro = new string[200];
+            int nOperacoes = 0;
             while (true)
             {
                 Console.Clear();
@@ -13,11 +15,12 @@
                 Console.WriteLine("3 - Multiplicação");
                 Console.WriteLine("4 - Divisão");
                 Console.WriteLine("5 - Tabuada de um número");
+                Console.WriteLine("6 - Visualizar as operações");
 
                 Console.WriteLine("\ns - Sair");
 
                 string Operacao = Console.ReadLine();
-                if (Operacao != "1" && Operacao != "2" && Operacao != "3" && Operacao != "4" && Operacao != "5" && Operacao != "s")
+                if (Operacao != "1" && Operacao != "2" && Operacao != "3" && Operacao != "4" && Operacao != "5" && Operacao != "s" && Operacao != "6")
                 {
                     Console.WriteLine("Opção inválida");
                     Console.ReadLine();
@@ -32,40 +35,53 @@
                 {
                     Console.Write("Digite o número da tabuada: ");
                     int tabuada = Convert.ToInt32(Console.ReadLine());
-
-
-
                     for (int numero = 1; numero <= 10; numero++)
                     {
                         Console.WriteLine(numero + " X " + tabuada + " = " + numero * tabuada);
                     }
+                    registro[nOperacoes] = ($"Tabuada do {tabuada}");
+                    nOperacoes++;
                     Console.ReadLine();
                     continue;
                 }
+                if (Operacao == "6")
+                {
+                    for (int i = 0; i < nOperacoes; i++)
+                    {
+                        Console.WriteLine(registro[i]);
+                    }
+                    Console.ReadLine();
+                    continue;
 
-                //if
+                }
+
                 Console.WriteLine("Digite o primeiro numero");
                 int PrimeiroN = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("Digite o segundo numero");
                 int SegundoN = Convert.ToInt32(Console.ReadLine());
                 int Resultado;
 
-
-
                 if (Operacao == "1")
                 {
                     Resultado = PrimeiroN + SegundoN;
                     Console.WriteLine("Resultado: " + Resultado);
+                    registro[nOperacoes] = ($"{PrimeiroN} + {SegundoN} = {Resultado}");
+                    nOperacoes++;
+
                 }
                 if (Operacao == "2")
                 {
                     Resultado = PrimeiroN - SegundoN;
                     Console.WriteLine("Resultado: " + Resultado);
+                    registro[nOperacoes] = ($"{PrimeiroN} - {SegundoN} = {Resultado}");
+                    nOperacoes++;
                 }
                 if (Operacao == "3")
                 {
                     Resultado = PrimeiroN * SegundoN;
                     Console.WriteLine("Resultado: " + Resultado);
+                    registro[nOperacoes] = ($"{PrimeiroN} * {SegundoN} = {Resultado}");
+                    nOperacoes++;
                 }
                 if (Operacao == "4")
                 {
@@ -78,8 +94,11 @@
 
                     Decimal ResultadoDeci = Convert.ToDecimal(PrimeiroN) / Convert.ToDecimal(SegundoN);
                     Console.WriteLine("Resultado: " + Math.Round(ResultadoDeci, 2));
+                    registro[nOperacoes] = ($"{PrimeiroN} / {SegundoN} = {ResultadoDeci}");
+                    nOperacoes++;
                 }
-                Console.ReadLine();
+
+                    Console.ReadLine();
             }
         }
     }
